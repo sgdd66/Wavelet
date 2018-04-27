1438d2d655b22a5828e8bfdd0224a4fa910112db
SGD <1770480953@qq.com>
2018-04-27 11:17:45 +0800

    4/27

---
+++ MyWavelet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+class wavelet(object):
+    def __init__(self):
+        pass
+
+    def pas(self,h,kk):
+        """"""
